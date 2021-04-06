77a5d9271a7c6c707075054976f19c9d6db38c54
Natasha Zebrev <zebre001@morris.umn.edu>
2021-04-06 17:54:10 -0500

    Add delete word type step to the Karma testing
    Adds a step in the karma test to delete the word type field that was added because the word type is now necessary once added in order to be able to add the contextpack.
    Co-authored-by: 
    Kyle Day <day00090@morris.umn.edu>
    Jeffrey Miller<mill7707@morris.umn.edu>
    Jadyn Sondrol <Sondr037@morris.umn.edu>

--- client/cypress/support/add-contextpack.po.ts
+++ client/cypress/support/add-contextpack.po.ts
@@ -1,6 +1,8 @@
+import { event } from 'cypress/types/jquery';
 import {ContextPack} from 'src/app/contextpacks/contextpack';
 
 export class AddPackPage {
+  contextPack: ContextPack;
   navigateTo() {
     return cy.visit('/edit');
   }
@@ -13,12 +15,12 @@ export class AddPackPage {
     return cy.get('[data-test="confirmAddPackButton"]');
   }
 
-  selectMatSelectValue(select: Cypress.Chainable, value: string) {
-    // Find and click the drop down
-    return select.click({ multiple: true, force:true })
-      // Select and click the desired value from the resulting menu
-      .get(`mat-option[value="${value}"]`).click({ multiple: true, force:true });
-  }
+  // selectMatSelectValue(select: Cypress.Chainable, value: string) {
+  //   // Find and click the drop down
+  //   return select.click({ multiple: true, force:true })
+  //     // Select and click the desired value from the resulting menu
+  //     .get(`mat-option[value="${value}"]`).click({ multiple: true, force:true });
+  // }
 
   getFormField(fieldName: string) {
     return cy.get(`mat-form-field [formcontrolname=${fieldName}]`);
@@ -28,7 +30,12 @@ export class AddPackPage {
     return cy.get('.add-wordlist-button').click({force: true});
   }
   addPosArray(pos: string){
+    //.type('farm words', {force: true})
     return cy.get(`.add-${pos}-button`).click({force: true});
+  }
+  deletePosArray(pos: string){
+    //.type('farm words', {force: true})
+    return cy.get(`.delete-${pos}-btn`).click({force: true});
   }
   showJson(){
     return cy.get('[data-test="showJsonButton"]').click({force: true});
@@ -40,20 +47,25 @@ export class AddPackPage {
 
 
 
+
   addPack(newPack: ContextPack) {
     this.getFormField('name').type(newPack.name);
-    this.getFormField('enabled').click({force: true});
+    // this.getFormField('enabled').click({force: true});
     this.addWordlist();
     this.addPosArray('noun');
+    this.deletePosArray('noun');
     this.addPosArray('verb');
+    this.deletePosArray('verb');
     this.addPosArray('adj');
+    this.deletePosArray('adj');
     this.addPosArray('misc');
+    this.deletePosArray('misc');
     if (newPack.wordlists) {
       this.getFormField('name').then(els => {
         [...els].forEach(el => cy.wrap(el).type('horsies', {force:true}));
       });
     }
-    this.selectMatSelectValue(this.getFormField('enabled'), newPack.enabled.toString());
+    // this.selectMatSelectValue(this.getFormField('enabled'), newPack.enabled.toString());
     return this.addPackButton().click({ multiple: true, force:true });
   }
 }
