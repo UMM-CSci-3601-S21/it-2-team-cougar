ca02ca76e0d16a9a0a21ac61bfa441603c97394a
Natasha Zebrev <zebre001@morris.umn.edu>
2021-04-06 19:47:44 -0500

    Add enabled status icons and tests for the Enabled display icons
    Adds icons that display the enabled status of a contextpack and adds tests and validations to the enabled icons that display the enabled status.
    Co-authored-by: 
    Kyle Day <day00090@morris.umn.edu>
    Jeffrey Miller<mill7707@morris.umn.edu>
    Jadyn Sondrol <Sondr037@morris.umn.edu>

--- client/cypress/support/add-contextpack.po.ts
+++ client/cypress/support/add-contextpack.po.ts
@@ -1,5 +1,6 @@
 import { event } from 'cypress/types/jquery';
 import {ContextPack} from 'src/app/contextpacks/contextpack';
+import { threadId } from 'worker_threads';
 
 export class AddPackPage {
   contextPack: ContextPack;
@@ -15,12 +16,12 @@ export class AddPackPage {
     return cy.get('[data-test="confirmAddPackButton"]');
   }
 
-  // selectMatSelectValue(select: Cypress.Chainable, value: string) {
-  //   // Find and click the drop down
-  //   return select.click({ multiple: true, force:true })
-  //     // Select and click the desired value from the resulting menu
-  //     .get(`mat-option[value="${value}"]`).click({ multiple: true, force:true });
-  // }
+  selectMatSelectValue(select: Cypress.Chainable, value: string) {
+    // Find and click the drop down
+    return select.click({ multiple: true, force:true })
+      // Select and click the desired value from the resulting menu
+      .get(`mat-option[value="${value}"]`).click({ multiple: true, force:true });
+  }
 
   getFormField(fieldName: string) {
     return cy.get(`mat-form-field [formcontrolname=${fieldName}]`);
@@ -50,7 +51,7 @@ export class AddPackPage {
 
   addPack(newPack: ContextPack) {
     this.getFormField('name').type(newPack.name);
-    // this.getFormField('enabled').click({force: true});
+    this.getFormField('enabled').click({force: true});
     this.addWordlist();
     this.addPosArray('noun');
     this.deletePosArray('noun');
@@ -65,7 +66,7 @@ export class AddPackPage {
         [...els].forEach(el => cy.wrap(el).type('horsies', {force:true}));
       });
     }
-    // this.selectMatSelectValue(this.getFormField('enabled'), newPack.enabled.toString());
+    this.selectMatSelectValue(this.getFormField('enabled'), newPack.enabled.toString());
     return this.addPackButton().click({ multiple: true, force:true });
   }
 }
